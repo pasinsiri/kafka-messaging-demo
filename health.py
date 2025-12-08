a0e6cdbd732d994f82c53e4f8bb40fae3235d793
pasinsiri <pasin_plp0206@hotmail.com>
2025-12-08 19:52:49 +0700

    initialize health.py

---
+++ health.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/health")
+def health():
+    return "Kafka is alive", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
